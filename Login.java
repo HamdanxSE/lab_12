12dc4796a842aee6faff2cb56663de315976d3a9
Hamdan <f223678@cfd.nu.edu.pk>
2024-11-14 21:14:43 +0500

    partner A, Commit 1

--- Login.java
+++ Login.java
@@ -1,6 +1,7 @@
 // Login.java
 public class Login {
 	public boolean authenticate(String username, String password) {
+		System.out.println("Authenticating user...");
 		return username.equals("admin") && password.equals("password123");
 	}
 }
