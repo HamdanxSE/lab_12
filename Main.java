dcc845a084399bf46500bef356480cd9aa7d55fd
Hamdan <f223678@cfd.nu.edu.pk>
2024-11-14 22:35:49 +0500

    1. change in main.Partner A. .

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
-public static void main(String[] args) {
-System.out.println("Main branch update.");
-}
+	public static void main(String[] args) {
+		System.out.println("Feature A work in progress.");
+		System.out.println("Main branch update.");
+	}
 }
